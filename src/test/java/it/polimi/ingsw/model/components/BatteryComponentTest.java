db891b0dbe8ca0159fb48a53c13b3ac3d4908fef
alesar1 <54070865+alesar1@users.noreply.github.com>
2025-03-13 23:36:30 +0100

    Add some tests

---
+++ src/test/java/it/polimi/ingsw/model/components/BatteryComponentTest.java
@@ -0,0 +1,48 @@
+package it.polimi.ingsw.model.components;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BatteryComponentTest {
+
+	@Test
+	void getConnectors() {
+	}
+
+	@Test
+	void getRotation() {
+	}
+
+	@Test
+	void verify() {
+	}
+
+	@Test
+	void getConnector() {
+	}
+
+	@Test
+	void getCoords() {
+	}
+
+	@Test
+	void check() {
+	}
+
+	@Test
+	void getContains() {
+	}
+
+	@Test
+	void getCapacity() {
+	}
+
+	@Test
+	void takeOne() {
+	}
+
+	@Test
+	void putOne() {
+	}
+}
