62de2aca2fc939c3849c48633a818abf7acce9e7
alesar1 <54070865+alesar1@users.noreply.github.com>
2025-03-16 18:25:47 +0100

    Continue AdventureCards
    Co-authored-by: Dingodaus <Dingodaus@users.noreply.github.com>
    Co-authored-by: filouwu <filouwu@users.noreply.github.com>

--- src/main/java/it/polimi/ingsw/model/adventure_cards/Planet.java
+++ src/main/java/it/polimi/ingsw/model/adventure_cards/Planet.java
@@ -1,9 +1,22 @@
 package it.polimi.ingsw.model.adventure_cards;
 
 public class Planet {
-    int red_material;
-    int blue_material;
-    int green_material;
-    int yellow_material;
-    boolean is_visited;
+    private int red_material, blue_material, green_material, yellow_material;
+    private boolean visited;
+
+    public int getBlueMaterial(){
+        return blue_material;
+    }
+    public int getGreenMaterial(){
+        return green_material;
+    }
+    public int getYellowMaterial(){
+        return yellow_material;
+    }
+    public int getRedMaterial(){
+        return red_material;
+    }
+    public boolean isVisited(){
+        return visited;
+    } 
 }
--- src/main/java/it/polimi/ingsw/model/adventure_cards/PlanetCard.java
+++ src/main/java/it/polimi/ingsw/model/adventure_cards/PlanetCard.java
@@ -1,26 +1,24 @@
 package it.polimi.ingsw.model.adventure_cards;
+
 import java.util.ArrayList;
 
-import it.polimi.ingsw.model.player.Player;
+import it.polimi.ingsw.model.player.*;
 
 public class PlanetCard extends Card {
-    public PlanetCard(int id){ //costruttore
-      
-    }
-    ArrayList<Planet> planets = new ArrayList<Planet>();
-    int days_spent; 
+	public PlanetCard(int id) { // costruttore
 
-    void visit(PlayerList){ 
-        /*for Player in PlayerList{
-            chiedi di atterrare
-            if yes 
-                load resources  // load goods on ship
-                player.position -= days_spent
-        }*/
-        for (int i=0; i< Playerlist.size(); i++){
-            if(chiedi di atterrare)
-                //load resources
-                player.position -= days_spent
-        }
-    }
+	}
+
+	ArrayList<Planet> planets = new ArrayList<Planet>();
+	int days_spent;
+
+	void visit(PlayerColor current_player/* planche.getFirstPlayer() */) {
+		/*
+		 * chiedi di atterrare
+		 * if yes
+		 * load resources // load goods on ship
+		 * player.position -= days_spent
+		 */
+		visit(Planche.getNextPlayer(current_player));
+	}
 }
