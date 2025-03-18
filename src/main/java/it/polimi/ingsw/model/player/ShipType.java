3f5feddae8e4e0b44db8285eae4c2173607f161a
Matteo Scuratti <teoscuraa@gmail.com>
2025-03-18 17:31:58 +0100

    removed frivulous todo

--- src/main/java/it/polimi/ingsw/model/player/ShipType.java
+++ src/main/java/it/polimi/ingsw/model/player/ShipType.java
@@ -26,7 +26,6 @@ public enum ShipType{
 		return this.width;
 	}
 
-	//TODO change to use ShipCoords
 	public int[] getShape(){
 		if(this.level==1){
 			return new int[]{0, 1, 2, 4, 5, 6, 7, 8 , 12, 13, 14, 20, 21, 27, 28, 31, 34};
