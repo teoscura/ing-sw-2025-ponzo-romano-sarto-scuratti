19c131f7b340a291eff45bf460f2af7f3f7cdb85
Matteo Scuratti <teoscuraa@gmail.com>
2025-03-18 15:28:18 +0100

    Removed annoying todo

--- src/main/java/it/polimi/ingsw/model/adventure_cards/EpidemicCard.java
+++ src/main/java/it/polimi/ingsw/model/adventure_cards/EpidemicCard.java
@@ -3,8 +3,7 @@ package it.polimi.ingsw.model.adventure_cards;
 import it.polimi.ingsw.model.player.iSpaceShip;
 
 public class EpidemicCard extends Card{
-    //TODO: lo fa matte(it's a joke)
-
+    
     public EpidemicCard(int id){
         super(id);
         //TODO
