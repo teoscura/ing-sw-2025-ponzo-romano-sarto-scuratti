a7c57a81750dfacaa9ac45b9940128783e756b21
filippo <romanofilippo7@gmail.com>
2025-03-18 15:40:07 +0100

    test BaseComponentTest and ShieldComponent (to_finish)

--- src/test/java/it/polimi/ingsw/model/components/BaseComponentTest.java
+++ src/test/java/it/polimi/ingsw/model/components/BaseComponentTest.java
@@ -1,17 +1,33 @@
 package it.polimi.ingsw.model.components;
 
+import it.polimi.ingsw.model.components.enums.ComponentRotation;
+import it.polimi.ingsw.model.components.enums.ConnectorType;
+import it.polimi.ingsw.model.components.enums.ShieldType;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BaseComponentTest {
 
+	private BaseComponent Base;
+
+
 	@Test
-	void getConnectors() {
+	void getConnectors(){
+		assertNotNull(Base.getConnectors());
 	}
 
 	@Test
 	void getRotation() {
+			ConnectorType[] connectors = new ConnectorType[4];
+			Base = new ShieldComponent(connectors, ComponentRotation.U000, ShieldType.NONE);
+			assertEquals(0, Base.getRotation().getShift());
+			Base = new ShieldComponent(connectors, ComponentRotation.U090, ShieldType.NONE);
+			assertEquals(1, Base.getRotation().getShift());
+			Base = new ShieldComponent(connectors, ComponentRotation.U180, ShieldType.NONE);
+			assertEquals(2, Base.getRotation().getShift());
+			Base = new StructuralComponent(connectors, ComponentRotation.U270);
+			assertEquals(3, Base.getRotation().getShift());
 	}
 
 	@Test
@@ -20,6 +36,19 @@ class BaseComponentTest {
 
 	@Test
 	void getConnector() {
+		ConnectorType[] connector_test_connectors = new ConnectorType[4];
+		connector_test_connectors[0] = ConnectorType.EMPTY;
+		connector_test_connectors[1] = ConnectorType.UNIVERSAL;
+		connector_test_connectors[2] = ConnectorType.SINGLE_CONNECTOR;
+		connector_test_connectors[3] = ConnectorType.DOUBLE_CONNECTOR;
+		BaseComponent connector_test_component = new StructuralComponent(connector_test_connectors, ComponentRotation.U000);
+		assertEquals(ConnectorType.EMPTY, connector_test_component.getConnector(ComponentRotation.U000));
+		connector_test_component = new StructuralComponent(connector_test_connectors, ComponentRotation.U090);
+		assertEquals(ConnectorType.DOUBLE_CONNECTOR, connector_test_component.getConnector(ComponentRotation.U000));
+		connector_test_component = new StructuralComponent(connector_test_connectors, ComponentRotation.U180);
+		assertEquals(ConnectorType.SINGLE_CONNECTOR, connector_test_component.getConnector(ComponentRotation.U000));
+		connector_test_component = new StructuralComponent(connector_test_connectors, ComponentRotation.U270);
+		assertEquals(ConnectorType.UNIVERSAL, connector_test_component.getConnector(ComponentRotation.U000));
 	}
 
 	@Test
--- src/test/java/it/polimi/ingsw/model/components/ShieldComponentTest.java
+++ src/test/java/it/polimi/ingsw/model/components/ShieldComponentTest.java
@@ -1,17 +1,24 @@
 package it.polimi.ingsw.model.components;
 
+import it.polimi.ingsw.model.components.enums.ComponentRotation;
+import it.polimi.ingsw.model.components.enums.ConnectorType;
+import it.polimi.ingsw.model.components.enums.ShieldType;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShieldComponentTest {
 
+	private ShieldComponent shield;
+
 	@Test
-	void getConnectors() {
+	void getConnectors() { //controlla tutte le connessioni intorno
+
 	}
 
 	@Test
-	void getRotation() {
+	void getRotation(){
+
 	}
 
 	@Test
@@ -27,22 +34,34 @@ class ShieldComponentTest {
 	}
 
 	@Test
-	void check() {
+	void check(){
+
 	}
 
 	@Test
-	void turnOn() {
+	void turnOn(){
+		shield.turnOn();
+		assertTrue(shield.getPowered());
 	}
 
 	@Test
 	void turnOff() {
+		shield.turnOff();
+		assertFalse(shield.getPowered());
 	}
 
 	@Test
 	void getPowered() {
+		shield.turnOff();
+		assertFalse(shield.getPowered());
+		shield.turnOn();
+		assertTrue(shield.getPowered());
 	}
 
 	@Test
 	void getShield() {
+		/*assertEquals(ShieldType.NONE, shield.getShield());
+		shield.turnOn();
+		assertEquals(ShieldType.NE, shield.getShield());*/
 	}
 }
