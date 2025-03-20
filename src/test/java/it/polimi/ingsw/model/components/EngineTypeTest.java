4be1d87083f3c7fe81ddb3ddd0a887c19c563379
Dingodaus <davide.ponzo2003@gmail.com>
2025-03-21 00:50:52 +0100

    finished EngineTypeTest, StorageComponentTest, StorageTypeTest, momentarily finished ShieldComponentTest

--- src/test/java/it/polimi/ingsw/model/components/EngineTypeTest.java
+++ src/test/java/it/polimi/ingsw/model/components/EngineTypeTest.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.model.components;
 
+import it.polimi.ingsw.model.components.enums.ComponentRotation;
+import it.polimi.ingsw.model.components.enums.ConnectorType;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,62 +9,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class EngineTypeTest {
 
 	@Test
-	void name() {
-	}
-
-	@Test
-	void ordinal() {
-	}
-
-	@Test
-	void testToString() {
-	}
-
-	@Test
-	void testEquals() {
-	}
-
-	@Test
-	void testHashCode() {
-	}
-
-	@Test
-	void testClone() {
-	}
-
-	@Test
-	void compareTo() {
-	}
-
-	@Test
-	void getDeclaringClass() {
-	}
-
-	@Test
-	void describeConstable() {
-	}
-
-	@Test
-	void valueOf() {
-	}
-
-	@Test
-	void testFinalize() {
-	}
-
-	@Test
 	void getMaxPower() {
+		EngineType maxpower_engine_type1 = EngineType.SINGLE;
+		EngineType maxpower_engine_type2 = EngineType.DOUBLE;
+		assertEquals(1 , maxpower_engine_type1.getMaxPower());
+		assertEquals(2 , maxpower_engine_type2.getMaxPower());
 	}
 
 	@Test
 	void getPowerable() {
+		EngineType powerable_engine_type1 = EngineType.SINGLE;
+		EngineType powerable_engine_type2 = EngineType.DOUBLE;
+        assertFalse(powerable_engine_type1.getPowerable());
+		assertTrue(powerable_engine_type2.getPowerable());
 	}
 
-	@Test
-	void values() {
-	}
-
-	@Test
-	void testValueOf() {
-	}
 }
--- src/test/java/it/polimi/ingsw/model/components/ShieldComponentTest.java
+++ src/test/java/it/polimi/ingsw/model/components/ShieldComponentTest.java
@@ -3,65 +3,57 @@ package it.polimi.ingsw.model.components;
 import it.polimi.ingsw.model.components.enums.ComponentRotation;
 import it.polimi.ingsw.model.components.enums.ConnectorType;
 import it.polimi.ingsw.model.components.enums.ShieldType;
+import it.polimi.ingsw.model.components.exceptions.AlreadyPoweredException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ShieldComponentTest {
-
-	private ShieldComponent shield;
-
-	@Test
-	void getConnectors() { //controlla tutte le connessioni intorno
-
-	}
-
-	@Test
-	void getRotation(){
-
-	}
+class ShieldComponentTest { //temporary, to change after shield component modified
 
 	@Test
 	void verify() {
 	}
 
 	@Test
-	void getConnector() {
-	}
-
-	@Test
-	void getCoords() {
-	}
-
-	@Test
 	void check(){
-
 	}
 
 	@Test
 	void turnOn(){
-		shield.turnOn();
-		assertTrue(shield.getPowered());
+		ConnectorType[] connectors = {ConnectorType.UNIVERSAL, ConnectorType.UNIVERSAL, ConnectorType.UNIVERSAL, ConnectorType.UNIVERSAL};
+		ShieldComponent turnon_shield = new ShieldComponent(connectors, ComponentRotation.U000, ShieldType.NW);
+		turnon_shield.turnOn();
+		assertTrue(turnon_shield.getPowered());
+		try{
+			turnon_shield.turnOn();
+			fail("AlreadyPoweredException did not occur");
+		} catch (AlreadyPoweredException e){}
 	}
 
 	@Test
 	void turnOff() {
-		shield.turnOff();
-		assertFalse(shield.getPowered());
+		ConnectorType[] connectors = {ConnectorType.UNIVERSAL, ConnectorType.UNIVERSAL, ConnectorType.UNIVERSAL, ConnectorType.UNIVERSAL};
+		ShieldComponent turnoff_shield = new ShieldComponent(connectors, ComponentRotation.U000, ShieldType.NW);
+		turnoff_shield.turnOff();
+		assertFalse(turnoff_shield.getPowered());
 	}
 
 	@Test
 	void getPowered() {
-		shield.turnOff();
-		assertFalse(shield.getPowered());
-		shield.turnOn();
-		assertTrue(shield.getPowered());
+		ConnectorType[] connectors = {ConnectorType.UNIVERSAL, ConnectorType.UNIVERSAL, ConnectorType.UNIVERSAL, ConnectorType.UNIVERSAL};
+		ShieldComponent powered_shield = new ShieldComponent(connectors, ComponentRotation.U000, ShieldType.NW);
+		powered_shield.turnOff();
+		assertFalse(powered_shield.getPowered());
+		powered_shield.turnOn();
+		assertTrue(powered_shield.getPowered());
 	}
 
 	@Test
 	void getShield() {
-		/*assertEquals(ShieldType.NONE, shield.getShield());
-		shield.turnOn();
-		assertEquals(ShieldType.NE, shield.getShield());*/
+		ConnectorType[] connectors = {ConnectorType.UNIVERSAL, ConnectorType.UNIVERSAL, ConnectorType.UNIVERSAL, ConnectorType.UNIVERSAL};
+		ShieldComponent getshield_shield = new ShieldComponent(connectors, ComponentRotation.U000, ShieldType.NW);
+		assertEquals(ShieldType.NONE, getshield_shield.getShield());
+		getshield_shield.turnOn();
+		assertEquals(ShieldType.NW, getshield_shield.getShield());
 	}
 }
