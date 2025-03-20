56c389f03dae12dfd8390617dd8acc62d9cc31e9
Matteo Scuratti <teoscuraa@gmail.com>
2025-03-20 09:50:57 +0100

    Fixed rotation.

--- src/main/java/it/polimi/ingsw/model/components/BaseComponent.java
+++ src/main/java/it/polimi/ingsw/model/components/BaseComponent.java
@@ -68,7 +68,7 @@ public abstract class BaseComponent implements iBaseComponent, iVisitable{
 
     @Override
     public ConnectorType getConnector(ComponentRotation direction){
-        int shift = direction.getShift() + this.rotation.getShift();
+        int shift = direction.getShift() + (4-this.rotation.getShift());
         shift = shift % 4;
         return connectors[shift];
     }
