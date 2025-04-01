a932c6e682009cef014b9a841db05dc17b5d6959
Dingodaus <davide.ponzo2003@gmail.com>
2025-04-01 14:20:40 +0200

    started card tests

---
+++ src/test/java/it/polimi/ingsw/model/board/PlancheTest.java
@@ -0,0 +1,63 @@
+package it.polimi.ingsw.model.board;
+
+import it.polimi.ingsw.model.GameModeType;
+import it.polimi.ingsw.model.PlayerCount;
+import it.polimi.ingsw.model.player.PlayerColor;
+import it.polimi.ingsw.model.player.SpaceShip;
+import it.polimi.ingsw.model.player.iSpaceShip;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+class PlancheTest {
+
+    private Planche planche;
+    private iSpaceShip shipRed;
+    private iSpaceShip shipBlue;
+
+    @BeforeEach
+    void setUp() {
+        planche = new Planche(GameModeType.LVL2, PlayerCount.TWO);
+        shipRed = new SpaceShip(GameModeType.LVL2, PlayerColor.RED);
+        shipBlue = new SpaceShip(GameModeType.LVL2, PlayerColor.BLUE);
+    }
+
+    @Test
+    void getPlayerPosition() {
+        assertEquals(0, planche.getPlayerPosition(shipRed));
+        assertEquals(0, planche.getPlayerPosition(shipBlue));
+    }
+
+    @Test
+    void getPlayersAt() {
+        planche.movePlayer(shipBlue, 1);
+        assertEquals(PlayerColor.RED, planche.getPlayersAt(0));
+        assertEquals(PlayerColor.BLUE, planche.getPlayersAt(1));
+    }
+
+    @Test
+    void movePlayer() {
+        planche.movePlayer(shipRed, 3);
+        planche.movePlayer(shipBlue, 2);
+        planche.movePlayer(shipBlue, 1);
+        assertEquals(4, planche.getPlayerPosition(shipBlue));
+        assertEquals(3, planche.getPlayerPosition(shipRed));
+
+        planche.movePlayer(shipBlue, -2);
+        planche.movePlayer(shipRed, -1);
+
+        assertEquals(2, planche.getPlayerPosition(shipRed));
+        assertEquals(1, planche.getPlayerPosition(shipBlue));
+    }
+
+    @Test
+    void getOrder() {
+        planche.movePlayer(shipRed, 3);
+        planche.movePlayer(shipBlue, 1);
+
+        List<PlayerColor> order = planche.getOrder();
+        assertEquals(List.of(PlayerColor.BLUE, PlayerColor.RED), order);
+    }
+}
