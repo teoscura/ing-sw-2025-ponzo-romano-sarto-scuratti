92c03c378650ef53e1f2d3d9e6f9557c13ddf804
Matteo Scuratti <teoscuraa@gmail.com>
2025-04-29 17:36:09 +0200

    pushed new verify functions

--- src/main/java/it/polimi/ingsw/model/player/VerifyResult.java
+++ src/main/java/it/polimi/ingsw/model/player/VerifyResult.java
@@ -2,28 +2,9 @@ package it.polimi.ingsw.model.player;
 
 public enum VerifyResult {
 	
-	GOOD_COMP (1),
-	UNCHECKED (false),
-	NOT_LNKED (-1),
-	BRKN_COMP (-2);
-
-	private int result = 0;
-	private boolean checked = true;
-
-	VerifyResult(int result){
-		this.result = result;
-	}
-
-	VerifyResult(boolean checked){
-		this.checked = checked;
-	}
-
-	public int getResult(){
-		return this.result;
-	}
-
-	public boolean getChecked(){
-		return this.checked;
-	}
+	GOOD_COMP,
+	BRKN_COMP,
+	UNCHECKED,
+	NOT_LNKED,
 
 }
