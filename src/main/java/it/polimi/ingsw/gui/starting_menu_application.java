7c9fcb878fea42cb4535e606d35e247d1de63cb7
Dingodaus <davide.ponzo2003@gmail.com>
2025-05-05 11:59:28 +0200

    i love file paths

--- src/main/java/it/polimi/ingsw/gui/starting_menu_application.java
+++ src/main/java/it/polimi/ingsw/gui/starting_menu_application.java
@@ -1,21 +1,22 @@
 package it.polimi.ingsw.gui;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class starting_menu_application extends Application {
+import java.io.IOException;
 
-    //exports starting_menu_application;
+public class starting_menu_application extends Application {
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        GridPane grid = new GridPane();
-        Scene scene = new Scene(grid, 800, 600);
+    public void start(Stage primaryStage) throws IOException {
+
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("")));
         primaryStage.setScene(scene);
         primaryStage.show();
     }
