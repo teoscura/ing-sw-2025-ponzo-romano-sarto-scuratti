927ec2e7733816d2b982ce28bff4936e32c0037c
filippo <romanofilippo7@gmail.com>
2025-05-20 01:06:31 +0200

    CannonComponent DOC

--- src/main/java/it/polimi/ingsw/model/components/CannonComponent.java
+++ src/main/java/it/polimi/ingsw/model/components/CannonComponent.java
@@ -15,6 +15,14 @@ import it.polimi.ingsw.model.components.visitors.iVisitor;
 import it.polimi.ingsw.model.player.ShipCoords;
 import it.polimi.ingsw.model.player.SpaceShip;
 
+/**
+ * <h2>CannonComponent</h2>
+ * <p>
+ * This class represents a <strong>cannon</strong> component that can be attached to a {@link SpaceShip}.
+ * Cannons are used to shoot at enemies during specific adventure cards or game phases.
+ * Depending on their type, cannons may be powerable (require battery activation) or not.
+ * </p>
+ */
 public class CannonComponent extends BaseComponent {
 
 	private final double max_power;
@@ -47,6 +55,9 @@ public class CannonComponent extends BaseComponent {
 		v.visit(this);
 	}
 
+	/**Checks whether the cannon can be placed in the current position on the ship.
+	 *@param ship {@link SpaceShip} to which you want to add the cannon component
+	 */
 	@Override
 	public boolean verify(SpaceShip ship) {
 		ComponentRotation r = this.getRotation();
@@ -71,6 +82,12 @@ public class CannonComponent extends BaseComponent {
 		return tmp == ship.getEmpty() && super.verify(ship);
 	}
 
+	/**
+	 * turn on the cannon component
+	 *
+	 * @throws AlreadyPoweredException if the cannon is already powered.
+	 * @throws UnpowerableException if the cannon is unpowerable.
+	 */
 	public void turnOn() {
 		if (this.powered) throw new AlreadyPoweredException();
 		if (!this.powerable) throw new UnpowerableException();
@@ -98,12 +115,20 @@ public class CannonComponent extends BaseComponent {
 		return this.powerable;
 	}
 
+	/**
+	 * This adds the Cannon Component's coordinates to the {@link SpaceShip}
+	 * @param ship  {@link SpaceShip} to which you want to add the Cannon component
+	 */
 	@Override
 	public void onCreation(SpaceShip ship, ShipCoords coords) {
 		this.coords = coords;
 		if (this.powerable) ship.addPowerableCoords(coords);
 	}
 
+	/**
+	 * This removes the Cannon Component's coordinates from the {@link SpaceShip}
+	 * @param ship  {@link SpaceShip} to which you want to remove the Cannon component
+	 */
 	@Override
 	public void onDelete(SpaceShip ship) {
 		if (powerable) ship.delPowerableCoords(coords);
