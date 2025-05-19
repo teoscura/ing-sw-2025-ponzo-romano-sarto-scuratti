90c03718a7c11d4b319d7869c8c5a22dbdcc10ac
filippo <romanofilippo7@gmail.com>
2025-05-20 00:42:42 +0200

    BatteryComponent DOC

--- src/main/java/it/polimi/ingsw/model/components/BatteryComponent.java
+++ src/main/java/it/polimi/ingsw/model/components/BatteryComponent.java
@@ -12,7 +12,14 @@ import it.polimi.ingsw.model.components.exceptions.ContainerFullException;
 import it.polimi.ingsw.model.components.visitors.iVisitor;
 import it.polimi.ingsw.model.player.ShipCoords;
 import it.polimi.ingsw.model.player.SpaceShip;
-
+/**
+ * <h2>BatteryComponent</h2>
+ * <p>
+ * This class models a ship component that contains a rechargeable battery.
+ * It is used to store and distribute energy to components like <i>double engines</i>, <i>cannons</i>,
+ * and other modules that require energy to activate.
+ * </p>
+ */
 public class BatteryComponent extends BaseComponent {
 
 	private final int max;
@@ -42,14 +49,26 @@ public class BatteryComponent extends BaseComponent {
 		v.visit(this);
 	}
 
+	/**
+	 * Returns the current batteries contained in the battery component.
+	 * @return the current batteries contained
+	 */
 	public int getContains() {
 		return this.contains;
 	}
 
+	/**
+	 * Returns the maximum capacity of the battery component.
+	 * @return maximum capacity
+	 */
 	public int getCapacity() {
 		return this.max;
 	}
 
+	/**
+	 * Takes one battery from the battery component.
+	 * @throws ContainerEmptyException if the component is already empty.
+	 */
 	public void takeOne() {
 		if (contains == 0) {
 			throw new ContainerEmptyException();
@@ -57,6 +76,10 @@ public class BatteryComponent extends BaseComponent {
 		this.contains--;
 	}
 
+	/**
+	 * Adds one battery to the battery component.
+	 * @throws ContainerFullException if capacity is already max.
+	 */
 	public void putOne() {
 		if (contains == max) {
 			throw new ContainerFullException();
@@ -64,12 +87,20 @@ public class BatteryComponent extends BaseComponent {
 		contains++;
 	}
 
+	/**
+	 * This adds the Battery Component's coordinates to the {@link SpaceShip}
+	 * @param ship  {@link SpaceShip} to which you want to add the battery component
+	 */
 	@Override
 	public void onCreation(SpaceShip ship, ShipCoords coords) {
 		this.coords = coords;
 		ship.addBatteryCoords(this.coords);
 	}
 
+	/**
+	 * This removes the Battery Component's coordinates from the {@link SpaceShip}
+	 * @param ship  {@link SpaceShip} to which you want to remove the battery component
+	 */
 	@Override
 	public void onDelete(SpaceShip ship) {
 		ship.delBatteryCoords(this.coords);
