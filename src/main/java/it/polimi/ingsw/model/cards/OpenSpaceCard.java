646c413633bc5196a550ceab2d21ea044cffa61d
filippo <romanofilippo7@gmail.com>
2025-05-19 16:49:16 +0200

    OpenSpaceCard DOC

--- src/main/java/it/polimi/ingsw/model/cards/OpenSpaceCard.java
+++ src/main/java/it/polimi/ingsw/model/cards/OpenSpaceCard.java
@@ -5,6 +5,15 @@ import it.polimi.ingsw.model.cards.state.OpenSpaceState;
 import it.polimi.ingsw.model.player.Player;
 import it.polimi.ingsw.model.state.VoyageState;
 
+/**
+ * Represents the "Open Space" adventure card in the game.
+ * <p>
+ * This card requires each player to choose whether or not to turn on engines.
+ * If a player's spaceship has at least one engine powered on, they will move forward
+ * by a number of cells equal to their engine power.
+ * Otherwise, the player loses the game.
+ * </p>
+ */
 public class OpenSpaceCard extends Card {
 
 	protected OpenSpaceCard(int id) {
@@ -16,6 +25,15 @@ public class OpenSpaceCard extends Card {
 		return new OpenSpaceState(state, this);
 	}
 
+	/**
+	 * Applies the card's effect to the given player's ship.
+	 * If the ship has engine power, it moves forward.
+	 * Otherwise, the player is eliminated from the game.
+	 *
+	 * @param state The Current State
+	 * @param p The Player
+	 * @throws NullPointerException    if {@code state} or {@code p} is null
+	 */
 	public void apply(VoyageState state, Player p) {
 		if (state == null || p == null) throw new NullPointerException();
 		if (p.getSpaceShip().getEnginePower() > 0) {
