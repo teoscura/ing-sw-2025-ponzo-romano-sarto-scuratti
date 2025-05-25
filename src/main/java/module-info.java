0ca38614a4388eb9716a40971a50fef7c8c8dbbc
alesar1 <54070865+alesar1@users.noreply.github.com>
2025-05-25 18:53:03 +0200

    Fix module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,7 @@ module it.polimi.ingsw {
 	requires org.jline;
 
 	opens it.polimi.ingsw to javafx.fxml;
-	opens it.polimi.ingsw.view.gui to javafx.graphics;
+	opens it.polimi.ingsw.view.gui to javafx.graphics, javafx.fxml;
 	exports it.polimi.ingsw;
 	exports it.polimi.ingsw.view.gui;
 }
