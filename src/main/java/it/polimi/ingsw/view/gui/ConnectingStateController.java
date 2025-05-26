d8153060570692149588f16e8aaa8be795a8bfa2
alesar1 <54070865+alesar1@users.noreply.github.com>
2025-05-26 22:51:11 +0200

    Working TitleScreen and ConnectingState

--- src/main/java/it/polimi/ingsw/view/gui/ConnectingStateController.java
+++ src/main/java/it/polimi/ingsw/view/gui/ConnectingStateController.java
@@ -1,59 +1,83 @@
 package it.polimi.ingsw.view.gui;
 
+import it.polimi.ingsw.controller.client.connections.ConnectionType;
 import it.polimi.ingsw.controller.client.state.ConnectingState;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.MenuButton;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+
+import java.util.Objects;
 
 public class ConnectingStateController {
 
 	private final ConnectingState state;
 	private final GUIView view;
-	private String ip, port;
+	private String ip, port, connection_type;
 
 	@FXML
 	private TextField ip_field;
 	@FXML
 	private TextField port_field;
 	@FXML
-	private MenuButton connection_menu;
+	private ChoiceBox connection_menu;
 
 	public ConnectingStateController(ConnectingState state, GUIView view) {
 		this.state = state;
 		this.view = view;
 	}
-
-	@FXML
-	public void confirmIp(ActionEvent event) {
-		ip = ip_field.getText();
-		ip_field.setDisable(true);
-		port_field.setDisable(false);
-	}
-
-	@FXML
-	public void confirmPort(ActionEvent event) {
-		port = port_field.getText();
-		connection_menu.setDisable(false);
-	}
-
-	@FXML
-	public void confirmRMI(ActionEvent event) {
-
-		//state.connect(ip, port, "rmi");
-	}
-
-	@FXML
-	public void confirmTCP(ActionEvent event) {
-		//state.connect(ip, port, "tcp");
-	}
+//
+//	@FXML
+//	public void confirmIp(ActionEvent event) {
+//		ip = ip_field.getText();
+//		ip_field.setDisable(true);
+//		port_field.setDisable(false);
+//	}
+//
+//	@FXML
+//	public void confirmPort(ActionEvent event) {
+//		port = port_field.getText();
+//		connection_menu.setDisable(false);
+//	}
+//
+//	@FXML
+//	public void confirmRMI(ActionEvent event) {
+//
+//		//state.connect(ip, port, "rmi");
+//	}
+//
+//	@FXML
+//	public void confirmTCP(ActionEvent event) {
+//		//state.connect(ip, port, "tcp");
+//	}
+//
+//	@FXML
+//	protected void initialize() {
+//		ip_field.setOnAction(this::confirmIp);
+//		port_field.setOnAction(this::confirmPort);
+//		connection_menu.getItems().get(0).setOnAction(this::confirmRMI);
+//		connection_menu.getItems().get(1).setOnAction(this::confirmTCP);
+//	}
 
 	@FXML
 	protected void initialize() {
-		ip_field.setOnAction(this::confirmIp);
-		port_field.setOnAction(this::confirmPort);
-		connection_menu.getItems().get(0).setOnAction(this::confirmRMI);
-		connection_menu.getItems().get(1).setOnAction(this::confirmTCP);
+		connection_menu.getItems().add("TCP");
+		connection_menu.getItems().add("RMI");
+
+	}
+
+	@FXML
+	protected void connect() {
+		ip = ip_field.getText();
+		port = port_field.getText();
+		connection_type = connection_menu.getValue().toString();
+		if (Objects.equals(connection_type, "TCP")) {
+			state.connect(ip, Integer.parseInt(port), ConnectionType.SOCKET);
+		} else if (Objects.equals(connection_type, "RMI")) {
+			state.connect(ip, Integer.parseInt(port), ConnectionType.RMI);
+		} else {
+			System.out.println("Select either TCP or RMI");
+		}
+
 	}
 
 }
