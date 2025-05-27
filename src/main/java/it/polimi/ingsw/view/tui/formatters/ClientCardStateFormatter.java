9d7f993c9c79f8f0b7834fe23018394842d945d1
Matteo Scuratti <teoscuraa@gmail.com>
2025-05-27 12:18:25 +0200

    reformatted ending state

--- src/main/java/it/polimi/ingsw/view/tui/formatters/ClientCardStateFormatter.java
+++ src/main/java/it/polimi/ingsw/view/tui/formatters/ClientCardStateFormatter.java
@@ -23,176 +23,176 @@ public class ClientCardStateFormatter implements ClientCardStateVisitor {
 	@Override
 	public void show(ClientAwaitConfirmCardStateDecorator state) {
 		line.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.GREEN))
-				.append("Awaiting confirm from: ");
+			.append("Awaiting confirm from: ");
 		for (PlayerColor c : state.getAwaiting()) {
 			line.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(getColor(c)))
-					.append(c.toString())
-					.style(AttributedStyle.DEFAULT)
-					.append(" | ");
+				.append(c.toString())
+				.style(AttributedStyle.DEFAULT)
+				.append(" | ");
 		}
 	}
 
 	@Override
 	public void show(ClientBaseCardState state) {
 		line.style(AttributedStyle.BOLD.background(AttributedStyle.BLUE).foreground(AttributedStyle.WHITE))
-				.append("[Card id: " + state.getID() + " - " + state.getState() + "]")
-				.style(AttributedStyle.DEFAULT)
-				.append(" | ");
+			.append("[Card id: " + state.getID() + " - " + state.getState() + "]")
+			.style(AttributedStyle.DEFAULT)
+			.append(" | ");
 	}
 
 	@Override
 	public void show(ClientCargoPenaltyCardStateDecorator state) {
 		line.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
-				.append("Cargo penalty: ")
-				.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(this.getColor(state.getTurn())))
-				.append(state.getTurn().toString())
-				.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
-				.append(": [")
-				.append(String.format("%02d", state.getShipments()[0]))
-				.append(" |")
-				.style(AttributedStyle.BOLD.foreground(AttributedStyle.BLUE))
-				.append(String.format("%02d", state.getShipments()[1]))
-				.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
-				.append(" |")
-				.style(AttributedStyle.BOLD.foreground(AttributedStyle.GREEN))
-				.append(String.format("%02d", state.getShipments()[2]))
-				.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
-				.append(" |")
-				.style(AttributedStyle.BOLD.foreground(AttributedStyle.YELLOW))
-				.append(String.format("%02d", state.getShipments()[3]))
-				.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
-				.append(" |")
-				.style(AttributedStyle.BOLD.foreground(AttributedStyle.RED))
-				.append(String.format("%02d", state.getShipments()[4]))
-				.style(AttributedStyle.DEFAULT)
-				.append("] | ");
+			.append("Cargo penalty: ")
+			.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(this.getColor(state.getTurn())))
+			.append(state.getTurn().toString())
+			.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
+			.append(": [")
+			.append(String.format("%02d", state.getShipments()[0]))
+			.append(" |")
+			.style(AttributedStyle.BOLD.foreground(AttributedStyle.BLUE))
+			.append(String.format("%02d", state.getShipments()[1]))
+			.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
+			.append(" |")
+			.style(AttributedStyle.BOLD.foreground(AttributedStyle.GREEN))
+			.append(String.format("%02d", state.getShipments()[2]))
+			.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
+			.append(" |")
+			.style(AttributedStyle.BOLD.foreground(AttributedStyle.YELLOW))
+			.append(String.format("%02d", state.getShipments()[3]))
+			.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
+			.append(" |")
+			.style(AttributedStyle.BOLD.foreground(AttributedStyle.RED))
+			.append(String.format("%02d", state.getShipments()[4]))
+			.style(AttributedStyle.DEFAULT)
+			.append("] | ");
 	}
 
 	@Override
 	public void show(ClientCargoRewardCardStateDecorator state) {
 		line.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(getColor(state.getTurn())))
-				.append(state.getTurn().toString())
-				.style(AttributedStyle.DEFAULT)
-				.append(" | ")
-				.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
-				.append("Cargo reward: ")
-				.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(this.getColor(state.getTurn())))
-				.append(state.getTurn().toString())
-				.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
-				.append(": [")
-				.style(AttributedStyle.BOLD.foreground(AttributedStyle.BLUE))
-				.append(String.format("%02d", state.getShipments()[0]))
-				.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
-				.append(" | ")
-				.style(AttributedStyle.BOLD.foreground(AttributedStyle.GREEN))
-				.append(String.format("%02d", state.getShipments()[1]))
-				.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
-				.append(" | ")
-				.style(AttributedStyle.BOLD.foreground(AttributedStyle.YELLOW))
-				.append(String.format("%02d", state.getShipments()[2]))
-				.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
-				.append(" | ")
-				.style(AttributedStyle.BOLD.foreground(AttributedStyle.RED))
-				.append(String.format("%02d", state.getShipments()[3]))
-				.style(AttributedStyle.DEFAULT)
-				.append("] | ");
+			.append(state.getTurn().toString())
+			.style(AttributedStyle.DEFAULT)
+			.append(" | ")
+			.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
+			.append("Cargo reward: ")
+			.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(this.getColor(state.getTurn())))
+			.append(state.getTurn().toString())
+			.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
+			.append(": [")
+			.style(AttributedStyle.BOLD.foreground(AttributedStyle.BLUE))
+			.append(String.format("%02d", state.getShipments()[0]))
+			.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
+			.append(" | ")
+			.style(AttributedStyle.BOLD.foreground(AttributedStyle.GREEN))
+			.append(String.format("%02d", state.getShipments()[1]))
+			.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
+			.append(" | ")
+			.style(AttributedStyle.BOLD.foreground(AttributedStyle.YELLOW))
+			.append(String.format("%02d", state.getShipments()[2]))
+			.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
+			.append(" | ")
+			.style(AttributedStyle.BOLD.foreground(AttributedStyle.RED))
+			.append(String.format("%02d", state.getShipments()[3]))
+			.style(AttributedStyle.DEFAULT)
+			.append("] | ");
 	}
 
 	@Override
 	public void show(ClientCombatZoneIndexCardStateDecorator state) {
 		String sectioninfo = state.getSection().getPenalty() != CombatZonePenalty.SHOTS ? state.getSection().getPenalty() + ": " + state.getSection().getAmount() : state.getSection().getPenalty().toString();
 		line.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.RED))
-				.append("[" + state.getIndex() + "]: " + state.getSection().getCriteria() + "/" + sectioninfo + " ]")
-				.style(AttributedStyle.DEFAULT)
-				.append(" | ");
+			.append("[" + state.getIndex() + "]: " + state.getSection().getCriteria() + "/" + sectioninfo + " ]")
+			.style(AttributedStyle.DEFAULT)
+			.append(" | ");
 	}
 
 	@Override
 	public void show(ClientCreditsRewardCardStateDecorator state) {
 		line.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.YELLOW))
-				.append("Credits reward for ")
-				.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(this.getColor(state.getTurn())))
-				.append(state.getTurn().toString())
-				.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.YELLOW))
-				.append(" credits: " + state.getCredits() + " days: " + state.getDaysTaken())
-				.style(AttributedStyle.DEFAULT)
-				.append(" | ");
+			.append("Credits reward for ")
+			.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(this.getColor(state.getTurn())))
+			.append(state.getTurn().toString())
+			.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.YELLOW))
+			.append(" credits: " + state.getCredits() + " days: " + state.getDaysTaken())
+			.style(AttributedStyle.DEFAULT)
+			.append(" | ");
 	}
 
 	@Override
 	public void show(ClientCrewPenaltyCardStateDecorator state) {
 		line.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.MAGENTA))
-				.append("Crew penalty: ")
-				.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(this.getColor(state.getTurn())))
-				.append(state.getTurn().toString())
-				.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.MAGENTA))
-				.append(", missing: " + state.getCrewLost())
-				.style(AttributedStyle.DEFAULT)
-				.append(" | ");
+			.append("Crew penalty: ")
+			.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(this.getColor(state.getTurn())))
+			.append(state.getTurn().toString())
+			.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.MAGENTA))
+			.append(", missing: " + state.getCrewLost())
+			.style(AttributedStyle.DEFAULT)
+			.append(" | ");
 	}
 
 	@Override
 	public void show(ClientLandingCardStateDecorator state) {
 		line.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.CYAN))
-				.append("Landing: ")
-				.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(getColor(state.getTurn())))
-				.append(state.getTurn().toString())
-				.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.CYAN))
-				.append(": ");
+			.append("Landing: ")
+			.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(getColor(state.getTurn())))
+			.append(state.getTurn().toString())
+			.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.CYAN))
+			.append(": ");
 		if (state.getAvailable() == null) {
 			line.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.YELLOW))
-					.append("0: credits: " + state.getCredits() + " days: " + state.getDaysTaken() + " crew: " + state.getCrewNeeded());
+				.append("0: credits: " + state.getCredits() + " days: " + state.getDaysTaken() + " crew: " + state.getCrewNeeded());
 		} else {
 			if (state.getCrewNeeded() > 0) {
 				line.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
-						.append("0: [")
-						.style(AttributedStyle.BOLD.foreground(AttributedStyle.BLUE))
-						.append(String.format("%02d", state.getAvailable().get(0).getContains()[0]))
-						.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
-						.append("|")
-						.style(AttributedStyle.BOLD.foreground(AttributedStyle.GREEN))
-						.append(String.format("%02d", state.getAvailable().get(0).getContains()[1]))
-						.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
-						.append("|")
-						.style(AttributedStyle.BOLD.foreground(AttributedStyle.YELLOW))
-						.append(String.format("%02d", state.getAvailable().get(0).getContains()[2]))
-						.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
-						.append("|")
-						.style(AttributedStyle.BOLD.foreground(AttributedStyle.RED))
-						.append(String.format("%02d", state.getAvailable().get(0).getContains()[3]))
-						.style(AttributedStyle.DEFAULT)
-						.append("]")
-						.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.GREEN))
-						.append(" Days: " + state.getDaysTaken() + " Crew: " + state.getCrewNeeded());
+					.append("0: [")
+					.style(AttributedStyle.BOLD.foreground(AttributedStyle.BLUE))
+					.append(String.format("%02d", state.getAvailable().get(0).getContains()[0]))
+					.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
+					.append("|")
+					.style(AttributedStyle.BOLD.foreground(AttributedStyle.GREEN))
+					.append(String.format("%02d", state.getAvailable().get(0).getContains()[1]))
+					.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
+					.append("|")
+					.style(AttributedStyle.BOLD.foreground(AttributedStyle.YELLOW))
+					.append(String.format("%02d", state.getAvailable().get(0).getContains()[2]))
+					.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.WHITE))
+					.append("|")
+					.style(AttributedStyle.BOLD.foreground(AttributedStyle.RED))
+					.append(String.format("%02d", state.getAvailable().get(0).getContains()[3]))
+					.style(AttributedStyle.DEFAULT)
+					.append("]")
+					.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.GREEN))
+					.append(" Days: " + state.getDaysTaken() + " Crew: " + state.getCrewNeeded());
 			} else {
 				line.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.GREEN))
-						.append("Days: " + state.getDaysTaken() + " ");
+					.append("Days: " + state.getDaysTaken() + " ");
 				int i = 0;
 				for (Planet p : state.getAvailable()) {
 					if (p.getVisited()) {
 						line.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.RED))
-								.append(i + ": N/A ")
-								.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.GREEN))
-								.append("| ");
+							.append(i + ": N/A ")
+							.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.GREEN))
+							.append("| ");
 					} else {
 						line.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.GREEN))
-								.append(i + ": [")
-								.style(AttributedStyle.BOLD.foreground(AttributedStyle.BLUE))
-								.append(String.format("%02d", state.getAvailable().get(i).getContains()[0]))
-								.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.GREEN))
-								.append("|")
-								.style(AttributedStyle.BOLD.foreground(AttributedStyle.GREEN))
-								.append(String.format("%02d", state.getAvailable().get(i).getContains()[1]))
-								.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.GREEN))
-								.append("|")
-								.style(AttributedStyle.BOLD.foreground(AttributedStyle.YELLOW))
-								.append(String.format("%02d", state.getAvailable().get(i).getContains()[2]))
-								.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.GREEN))
-								.append("|")
-								.style(AttributedStyle.BOLD.foreground(AttributedStyle.RED))
-								.append(String.format("%02d", state.getAvailable().get(i).getContains()[3]))
-								.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.GREEN))
-								.append("] ");
+							.append(i + ": [")
+							.style(AttributedStyle.BOLD.foreground(AttributedStyle.BLUE))
+							.append(String.format("%02d", state.getAvailable().get(i).getContains()[0]))
+							.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.GREEN))
+							.append("|")
+							.style(AttributedStyle.BOLD.foreground(AttributedStyle.GREEN))
+							.append(String.format("%02d", state.getAvailable().get(i).getContains()[1]))
+							.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.GREEN))
+							.append("|")
+							.style(AttributedStyle.BOLD.foreground(AttributedStyle.YELLOW))
+							.append(String.format("%02d", state.getAvailable().get(i).getContains()[2]))
+							.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.GREEN))
+							.append("|")
+							.style(AttributedStyle.BOLD.foreground(AttributedStyle.RED))
+							.append(String.format("%02d", state.getAvailable().get(i).getContains()[3]))
+							.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.GREEN))
+							.append("] ");
 					}
 					i++;
 				}
@@ -204,37 +204,37 @@ public class ClientCardStateFormatter implements ClientCardStateVisitor {
 	@Override
 	public void show(ClientMeteoriteCardStateDecorator state) {
 		line.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.BRIGHT))
-				.append(state.getProjectile().getDimension() + " Meteor " + state.getProjectile().getDirection() + " " + normalizeOffset(state.getProjectile().getDirection().getShift(), state.getProjectile().getOffset()))
-				.style(AttributedStyle.DEFAULT)
-				.append(" | ");
+			.append(state.getProjectile().getDimension() + " Meteor " + state.getProjectile().getDirection() + " " + normalizeOffset(state.getProjectile().getDirection().getShift(), state.getProjectile().getOffset()))
+			.style(AttributedStyle.DEFAULT)
+			.append(" | ");
 	}
 
 	@Override
 	public void show(ClientNewCenterCardStateDecorator state) {
 		line.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.GREEN))
-				.append("Need new center from: ");
+			.append("Need new center from: ");
 		for (PlayerColor c : state.getAwaiting()) {
 			line.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(getColor(c)))
-					.append(c.toString())
-					.style(AttributedStyle.DEFAULT)
-					.append(" | ");
+				.append(c.toString())
+				.style(AttributedStyle.DEFAULT)
+				.append(" | ");
 		}
 	}
 
 	@Override
 	public void show(ClientProjectileCardStateDecorator state) {
 		line.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.MAGENTA))
-				.append(state.getProjectile().getDimension() + " Shot " + state.getProjectile().getDirection() + " " + normalizeOffset(state.getProjectile().getDirection().getShift(), state.getProjectile().getOffset()))
-				.style(AttributedStyle.DEFAULT)
-				.append(" | ");
+			.append(state.getProjectile().getDimension() + " Shot " + state.getProjectile().getDirection() + " " + normalizeOffset(state.getProjectile().getDirection().getShift(), state.getProjectile().getOffset()))
+			.style(AttributedStyle.DEFAULT)
+			.append(" | ");
 	}
 
 	@Override
 	public void show(ClientEnemyCardStateDecorator state) {
 		line.style(AttributedStyle.BOLD.background(AttributedStyle.BLACK).foreground(AttributedStyle.RED))
-				.append(" Enemy power: " + state.getPower() + " | Penalty: " + state.getPenalty() + "/" + (state.getAmount() > 0 ? state.getAmount() : ""))
-				.style(AttributedStyle.DEFAULT)
-				.append(" | ");
+			.append(" Enemy power: " + state.getPower() + " | Penalty: " + state.getPenalty() + "/" + (state.getAmount() > 0 ? state.getAmount() : ""))
+			.style(AttributedStyle.DEFAULT)
+			.append(" | ");
 	}
 
 	private int getColor(PlayerColor color) {
